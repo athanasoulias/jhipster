a4d3494991825ce77916385ad4b7ce775341b3a5
G.A <athanasoulias@gmail.com>
2021-04-08 01:52:39 +0300

    Initial version of mulos_psari generated by JHipster-7.0.1

---
+++ src/test/java/com/iss/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.iss;
+
+import com.iss.MulosPsariApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = MulosPsariApp.class)
+public @interface IntegrationTest {
+}
